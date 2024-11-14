774a6138ea37454ece7258b93e4d625089295d23
djns <130430285+DDJCS50@users.noreply.github.com>
2024-11-14 00:21:48 -0800

    Fix port
    Fixed port from string

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Message board app - listening on port ${PORT}!`);
 });
