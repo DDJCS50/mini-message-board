67891cd01bfcaf3c4af0ff00b6f999e987a7df61
djns <130430285+DDJCS50@users.noreply.github.com>
2024-11-14 00:54:25 -0800

    Remove public access
    Removed access to public directory

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const indexRouter = require("./routes/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
+// const assetsPath = path.join(__dirname, "public");
+// app.use(express.static(assetsPath));
 
 app.use(express.urlencoded({ extended: true }));
 
